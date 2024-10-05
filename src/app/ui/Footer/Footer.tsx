960b4a0fad299c3992f7697622570e66a4248e59
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2024-10-06 00:27:04 +0300

    Update Footer.tsx

--- src/app/ui/Footer/Footer.tsx
+++ src/app/ui/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex justify-center text-slate-500 text-sm my-10">
-      <Link href="https://github.com/anfepar" target="_blank">
-        {"Made with ❤️ by Felipe Pardo © 2023"}
+      <Link href="https://github.com/Ranamom/crypto-web" target="_blank">
+        {"❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️"}
       </Link>
     </footer>
   )
